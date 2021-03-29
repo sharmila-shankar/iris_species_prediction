decb17867dbbcd6f4b65bdad018931551c254654
Sharmila <shankarsharmi1@gmail.com>
2021-03-29 13:38:45 +0530

    allowed host localhost added

--- iris_proj/settings.py
+++ iris_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '6u7c52pz!-k3=9il70a-nrnpz*-j7mpek_8l_p^w1*nroly1)w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.128.91.151']
+ALLOWED_HOSTS = ['3.128.91.151', 'localhost']
 
 
 # Application definition
